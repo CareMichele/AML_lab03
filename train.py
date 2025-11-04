713b6f481747a1997a5671d1af90e8b5311f4d17
Michele Carena <s349483@studenti.polito.it>
2025-11-04 22:39:40 +0100

    changes

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 import torch
-from torch import nn
 from models.custom_net import CustomNet
-from data.data import train_loader, val_loader
-from eval import validate
 
 def train(epoch, model, train_loader, criterion, optimizer):
     model.train()
@@ -32,23 +29,5 @@ def train(epoch, model, train_loader, criterion, optimizer):
     train_loss = running_loss / len(train_loader)
     train_accuracy = 100. * correct / total
     print(f'Train Epoch: {epoch} Loss: {train_loss:.6f} Acc: {train_accuracy:.2f}%')
-    
-if __name__ == "__main__":
-    model = CustomNet().cuda()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    best_acc = 0
-
-    # Run the training process for {num_epochs} epochs
-    num_epochs = 10
-    for epoch in range(1, num_epochs + 1):
-        train(epoch, model, train_loader, criterion, optimizer)
-
-        # At the end of each training iteration, perform a validation step
-        val_accuracy = validate(model, val_loader, criterion)
-
-        # Best validation accuracy
-        best_acc = max(best_acc, val_accuracy)
-
-    print(f'Best validation accuracy: {best_acc:.2f}%')
+    return train_accuracy
